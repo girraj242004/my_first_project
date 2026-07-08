a699283fe1541996437111adc4c17f24ebb11155
girraj242004 <girrajagrawal2004@gmail.com>
2026-07-08 15:02:08 +0530

    Type conversion in JS

---
+++ type_conversion.js
@@ -0,0 +1,19 @@
+let score = 33
+let score1 = "33"
+
+console.log(typeof score)
+console.log(typeof score1)
+
+let valueInNumber = Number(score1)
+console.log(typeof valueInNumber)
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+
+let isLoggedIn = 1
+let isLoggedIn1 = Boolean(isLoggedIn)
+console.log(typeof isLoggedIn1)
+// 1 => true
+// 0 => false
+// "" => false
+// "abc" => true
