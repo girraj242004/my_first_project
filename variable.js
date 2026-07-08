a88e95b0e02c304571b2e25f626514083199b85c
girraj242004 <girrajagrawal2004@gmail.com>
2026-07-08 13:08:57 +0530

    Variable

---
+++ variable.js
@@ -0,0 +1,13 @@
+// const are immutable in nature
+const accountId = 123456;
+
+// let are mutable in nature
+let accountEmail = "girraj@google.com";
+
+// var are prefered not use because of issue in bloack scope and functinal scope
+var accountPassword =   "12345";
+
+// they show undefined 
+let accountCity ;
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
