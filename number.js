5017e46f37ad8d734ea896ef530c57304ef109c8
girraj242004 <girrajagrawal2004@gmail.com>
2026-07-09 10:41:44 +0530

    Numbers in js

---
+++ number.js
@@ -0,0 +1,15 @@
+const score = 400
+const balance = new Number(100)
+
+console.log(score);
+console.log(balance);
+
+console.log(balance.toString().length)
+
+console.log(balance.toFixed(2))
+
+const num = 123.8664
+console.log(num.toPrecision(4))
+ 
+const lakh = 100000
+console.log(lakh.toLocaleString())
