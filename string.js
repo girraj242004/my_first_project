e518076157d1011ef7963967aa5e39f59ca4fb1f
girraj242004 <girrajagrawal2004@gmail.com>
2026-07-09 10:24:36 +0530

    String

---
+++ string.js
@@ -0,0 +1,33 @@
+const name = "Girraj"
+let count = 50
+// console.log(`My name is ${name} and my count is ${count}`)
+
+const gameName = "girraj-ag-raw"
+
+// console.log(gameName[4])
+// console.log(gameName.length)
+
+// console.log(gameName.toUpperCase())
+// console.log(gameName.toLowerCase())
+
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'))
+
+// const newString = gameName.substring(0, 6)
+// console.log(newString)
+
+// const anotherString = gameName.slice(0, 6)
+// console.log(anotherString)
+
+// const str = "   girraj   "
+// console.log(str.trim())
+// console.log(str)
+
+const url = "https://www.girraj.com"
+console.log(url.replace("girraj", "bob"))
+console.log(url.includes("girraj"))
+const word = gameName.split('-')
+console.log(word.toString())
+console.log(gameName.replaceAll('-', ' '))
+
+
