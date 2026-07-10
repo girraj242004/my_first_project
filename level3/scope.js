30d390fbd3d5fbd891bd2d077312663ba3866e27
girraj242004 <girrajagrawal2004@gmail.com>
2026-07-10 15:22:46 +0530

    Scope in JS

---
+++ level3/scope.js
@@ -0,0 +1,18 @@
+let a = 10;
+const b =20
+
+
+
+if ( true){
+    let a = 100;
+    const b = 200;
+    var c = 300;
+    console.log(a)
+console.log(b)
+console.log(c)
+}
+
+
+console.log(a)
+console.log(b)
+console.log(c)
