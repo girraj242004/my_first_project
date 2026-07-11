df9b54c31db2d121d688a0207c95de8df65c149d
girraj242004 <girrajagrawal2004@gmail.com>
2026-07-11 11:27:20 +0530

    IIFE in js

---
+++ level3/iife.js
@@ -0,0 +1,9 @@
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    console.log(`DB conntected`)
+})();
+
+(function code(name){
+    console.log(`DB connected ${name}`)
+})("hitesh");
